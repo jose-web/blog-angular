121099dbb871e03d80a92d80bc80b7d1a809243a
jose <delossantosramosjose@gmail.com>
2022-08-21 02:19:10 +0200

    server.js added

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./dist/blog-angular'))
+
+app.get('/*', (req, res)=>{
+    res.sendFile('index.html', {root: 'dist/blog-angular'})
+})
+
+app.listen(process.env.PORT || 8080)
