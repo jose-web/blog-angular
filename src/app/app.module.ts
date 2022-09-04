8797efc50aa23b1ef836ce8d6234d5306a0857d2
jose <delossantosramosjose@gmail.com>
2022-09-04 15:46:05 +0200

    comments added in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+
+// PAGES
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
+
+    // COMPONENTS
     AppComponent,
     MenuComponent,
+
+    // PAGES
     LoginComponent,
     RegisterComponent
+    
   ],
   imports: [
     BrowserModule
